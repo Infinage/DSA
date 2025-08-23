e8a6625e7a89b01029846fac80fafc5e9c797f05
infinage <37257700+Infinage@users.noreply.github.com>
2025-08-23 23:27:00 +0530

    DSA helpers

---
+++ study/striver-A2Z/DSA.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <algorithm>
+#include <concepts>
+#include <queue>
+#include <initializer_list>
+#include <vector>
+
+template<std::integral T>
+struct HashVector {
+    inline std::size_t operator()(const std::vector<T> &vec) const {
+        std::hash<T> hasher; auto seed {vec.size()};
+        return std::ranges::fold_left(vec, seed, 
+            [&hasher](std::size_t acc, const T &n) {
+                acc ^= hasher(n) + 0x9e3779b9 + (acc << 6) + (acc >> 2);
+                return acc;
+            }
+        );
+    }
+};
+
+template<typename T = int>
+struct TreeNode {
+    T val {}; TreeNode *left {}, *right {};
+    ~TreeNode() { delete left; delete right; }
+
+    /*
+     * Function to create a Forest of Binary Tree Nodes from an array.
+     * Array must be in *level* order format.
+     * `null` values would be set to nullptr.
+     */
+    static TreeNode* from(std::initializer_list<T> array, const T &null) {
+        // Convert into an array of TreeNodes
+        std::vector<TreeNode*> forest(array.size());
+        std::transform(array.begin(), array.end(), forest.begin(), 
+            [&null](const T &val) { 
+                return val == null? nullptr: new TreeNode {val}; 
+            }
+        );
+
+        // Link the TreeNode to parent
+        std::size_t parent {}, N {forest.size()};
+        for (std::size_t curr {1}; curr < N; ++curr) {
+            if (curr % 2) forest[parent]->left = forest[curr];
+            else forest[parent++]->right = forest[curr];
+            while (parent < N && !forest[parent]) ++parent;
+        }
+
+        return forest.empty()? nullptr: forest[0];
+    }
+
+    /*
+     * Function to create a vector of values from a Root Tree node.
+     * Returned array would be in level order format.
+     * nullptr values would be set to `null`.
+     */
+    static std::vector<T> toVector(TreeNode *root, const T &null) {
+        std::vector<T> res; std::queue<TreeNode<T>*> queue; queue.push(root);
+        while (!queue.empty()) {
+            TreeNode *curr {queue.front()}; queue.pop();
+            if (!curr) res.emplace_back(null);
+            else {
+                queue.push(curr->left); queue.push(curr->right);
+                res.emplace_back(curr->val);
+            }
+        }
+        return res;
+    }
+};
